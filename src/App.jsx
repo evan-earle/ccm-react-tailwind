e2c83e4e8972dd47a632fea38988996ab11471ea
Evan Earle <evan.earle@gmail.com>
2023-11-14 13:45:09 -0400

    completed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
+import {
+  CustomerReviews,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Hero,
+  Subscribe,
+  SuperQuality,
+} from "./sections/index";
+import Nav from "./components/Nav";
+
 const App = () => (
   <main className="relative">
-    Nav{" "}
-    <section className="x1:padding-1 wide:padding-r padding-b">Hero</section>
-    <section className="padding">Popular Products</section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">Special Offer</section>
-    <section className="bg-pale-blue padding">Customer Reviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    <Nav />
+    <section className="x1:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
   </main>
 );
 
